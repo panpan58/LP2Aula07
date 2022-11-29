fd85b2b3f1223976ceacbb89f8d62bd824a1f321
António Branco <ajsbranco15@gmail.com>
2022-11-29 12:22:36 +0000

    complete final exercize

--- LINQQuery/Program.cs
+++ LINQQuery/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQQuery
 {
@@ -6,7 +9,23 @@ namespace LINQQuery
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] lines = File.ReadAllLines("roger1.txt");
+
+            int linesin30 = lines.Count(line => line.Length < 31);
+            double average = lines.Average(lines => lines.Length);
+            int linein120 = lines.Count(line => line.Length > 120);
+            IEnumerable<string> thewhys =
+                from line in lines 
+                where line.Contains('Y')
+                select line.Trim().Split()[0].ToUpper();
+
+
+            Console.WriteLine(linesin30);
+            Console.WriteLine(average);
+            Console.WriteLine(linein120);
+            foreach(string y in thewhys)
+                Console.WriteLine(y.ToString());
+            
         }
     }
 }
